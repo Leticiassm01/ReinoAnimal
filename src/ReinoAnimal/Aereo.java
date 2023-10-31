b134507d122719bc582813938e08f2ff98a78321
20201064010040 <20201064010040@CANG278280.ifrn.local>
2023-10-31 16:49:08 -0300

    atualização classe aerea

--- src/ReinoAnimal/Aereo.java
+++ src/ReinoAnimal/Aereo.java
@@ -1,5 +1,32 @@
 package ReinoAnimal;
 
-public class Aereo {
+public class Aereo extends Animal{
 
+	public Aereo(String especie, String sexo, String dataNascimento, String altitude) {
+		super(especie, sexo, dataNascimento);
+		// TODO Auto-generated constructor stub
+		this.altitude=altitude;
+		}
+	private String altitude;
+	public String getAltitude() {
+		return altitude;
+	}
+	public void setAltitude(String altitude) {
+		this.altitude = altitude;
+	}
+	
+	public void voar() {
+		System.out.println("O animal está voando");
+	}
+	
+	public void comer() {
+		System.out.println("O Animal aereo está comendo");
+	}
+	
+	public void perfil() {
+		super.perfil();
+		System.out.println("quantidade de animais" + contador);
+		System.out.println("O animal Aereo voa na altutede" + altitude);
+		
+	}
 }

7865a30f0686cf99d509274a4ec0072e803e5dc8
20201064010040 <20201064010040@192.168.56.1>
2023-11-01 17:29:40 -0300

    Classe aereo

--- src/ReinoAnimal/Aereo.java
+++ src/ReinoAnimal/Aereo.java
@@ -1,12 +1,29 @@
 package ReinoAnimal;
+/**
+ * 
+ * 
+ * Essa classe é responsavel pela especificação de um animal aereo.
+ * 
+ * 
+ * @author Leticia stephanie
+ *
+ */
 
 public class Aereo extends Animal{
+	
+	/**
+	 * Construtor da classe de animais aereos
+	 * @param especie
+	 * @param sexo
+	 * @param dataNascimento
+	 * @param altitude
+	 */
 
 	public Aereo(String especie, String sexo, String dataNascimento, String altitude) {
 		super(especie, sexo, dataNascimento);
 		// TODO Auto-generated constructor stub
 		this.altitude=altitude;
-		}
+	}
 	private String altitude;
 	public String getAltitude() {
 		return altitude;
@@ -16,17 +33,26 @@ public class Aereo extends Animal{
 	}
 	
 	public void voar() {
-		System.out.println("O animal está voando");
+		System.out.println("O animal aereo está voando");
 	}
+	/*
+	 * metodo que mostra o animal voando
+	 */
 	
 	public void comer() {
 		System.out.println("O Animal aereo está comendo");
 	}
+	/*
+	 * metodo que mostra que o animal esta comendo
+	 */
 	
 	public void perfil() {
 		super.perfil();
-		System.out.println("quantidade de animais" + contador);
+		System.out.println("quantidade de animais" + getContador());
 		System.out.println("O animal Aereo voa na altutede" + altitude);
 		
 	}
+	/*
+	 * metodo que mostra o perfil do animal aereo
+	 */
 }
